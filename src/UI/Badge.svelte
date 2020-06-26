3ee4d2370479dcdcfe772590126a381f3b2f19b1
Brad Mehder <bmehder@gmail.com>
2020-06-26 11:26:25 +0200

    re-add Meetus project files

---
+++ src/UI/Badge.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { slide } from "svelte/transition";
+</script>
+
+<style>
+  span {
+    display: inline-block;
+    margin: 0 2.5rem;
+    border-radius: 3px;
+    border: 1px solid #cf0056;
+    background: #cf0056;
+    color: #fff;
+    padding: 0 0.5rem;
+    font-family: "Lato", sans-serif;
+    font-size: 0.8rem;
+  }
+</style>
+
+<span transition:slide>
+  <slot />
+</span>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,26 @@
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: #cf0056;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.2);
+  }
+  header h1 {
+    color: #fff;
+    font-family: "Roboto Slab", serif;
+    margin: 0;
+  }
+</style>
+
+<header>
+  <h1>
+    MeetUs v1
+    <em>(Non-writable)</em>
+  </h1>
+</header>
